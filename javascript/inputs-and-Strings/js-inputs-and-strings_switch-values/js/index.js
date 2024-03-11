ca0390ddaf551f52b701a18c6a891c688944e056
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-11 14:59:27 +0100

    Solving js-inputs-and-strings_switch-values-challenge

---
+++ javascript/inputs-and-Strings/js-inputs-and-strings_switch-values/js/index.js
@@ -0,0 +1,40 @@
+/*
+
+Switch the input values of the two input fields when clicking the button
+
+- [x] Add a second input element to the index.html
+- [x] Assign the second input element to a variable
+- [x] Add a third button (a switch button) to the index.html
+- [x] Assign the third button to a variable
+- [ ] Make sure that when the switch button is clicked, the values
+  of the two input fields are being switched
+- [ ] Make sure that all three buttons work on both input fields
+
+Hint: the HTML entity for the switch button is: &updownarrow
+
+*/
+
+const firstInput = document.querySelector('[data-js="first-input"]');
+const secondInput = document.querySelector('[data-js="second-input"]');
+const uppercaseButton = document.querySelector('[data-js="button-uppercase"]');
+const lowercaseButton = document.querySelector('[data-js="button-lowercase"]');
+const switchButton = document.querySelector('[data-js="button-switch"]');
+
+uppercaseButton.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toUpperCase();
+});
+
+lowercaseButton.addEventListener("click", () => {
+  firstInput.value = firstInput.value.toLowerCase();
+});
+
+switchButton.addEventListener("click", () => {
+  // Store the value of the first input
+  const temporaryValue = firstInput.value;
+
+  // Save the Input of the first value in the value of the second Input
+  firstInput.value = secondInput.value;
+
+  // Place the Input of the second value in the value of the first Input
+  secondInput.value = temporaryValue;
+});
