41b820c68757b0ec01930280940fbc7d5f23f23c
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-20 10:51:24 +0100

    Solving js-error-handling_calculator Challenge

---
+++ javascript/error-handling/js-error-handling_calculator/js/index.js
@@ -0,0 +1,34 @@
+console.clear();
+
+const form = document.querySelector("form");
+const output = form.querySelector("output");
+const errorPaasage = document.querySelector('[data-js="error"]');
+
+const operations = {
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => {
+    if (b === 0) {
+      throw new Error("Cannot divide by zero!");
+    }
+    return a / b;
+  },
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const firstNumber = Number(event.target.firstNumber.value);
+  const secondNumber = Number(event.target.secondNumber.value);
+  const operation = event.target.operation.value;
+
+  try {
+    const result = operations[operation](firstNumber, secondNumber);
+    output.innerText = result;
+    console.log(`${firstNumber} ${operation} ${secondNumber} equals ${result}`);
+  } catch (error) {
+    output.innerText = ""; // Clear output in case of error
+    console.error("An error occurred:", error.message);
+    errorPaasage.innerText = "Error: " + error.message;
+  }
+});
