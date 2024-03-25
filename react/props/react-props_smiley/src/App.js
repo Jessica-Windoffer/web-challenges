9bc183082ab47643f64f3363fe361b1245e684ac
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-25 16:10:15 +0100

    Solving react-props_smiley Challenge

---
+++ react/props/react-props_smiley/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </div>
+  );
+}
+
+function Smiley(props) {
+  // if the incoming parameter is happy, then (?) render ":-)",
+  // otherwise (:) render the given ":-(".
+  return <div>{props.isHappy === true ? ":-)" : ":-("}</div>;
+}
