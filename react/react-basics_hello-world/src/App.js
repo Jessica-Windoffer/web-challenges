e7810a6975906de7e9f642aeff7722141c656725
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-25 11:16:26 +0100

    Solving react-basics_hello-world Challenge

---
+++ react/react-basics_hello-world/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  // Vorher Status
+  // return <div>Say hello...</div>;
+
+  // Nachher Status
+  return <HelloWorld />;
+}
+
+// Nachher Status
+function HelloWorld() {
+  return <h1>Hello World!</h1>;
+}
