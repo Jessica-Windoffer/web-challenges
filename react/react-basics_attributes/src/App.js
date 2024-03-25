636124e154d42aec5c1bd79a78dc7450330157dc
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-25 11:59:03 +0100

    Solving react-basics_attributes Challenge

---
+++ react/react-basics_attributes/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return <Article />;
+}
+
+function Article() {
+  return (
+    <article>
+      <h2 className="article__title">This is a Headline for the article!</h2>
+      <label htmlFor="inputField">Put your input into the text field:</label>
+      <input id="inputField"></input>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/Lorem_ipsum"
+      >
+        Wikipedia Article regarding Lorem Ipsum
+      </a>
+    </article>
+  );
+}
